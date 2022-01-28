9936fdbaadd10e40de091e5d50027b2328aa8856
purusottam234 <purusottamadhikari234@gmail.com>
2022-01-28 08:53:46 +0545

    adding some configuration

---
+++ data.js
@@ -0,0 +1,18 @@
+modeule.exports = function (){
+    return{
+        categories = ['watersports','soccer','chess'],
+        products =[
+            {id:1, name:'kayak', category:'watersports', discription:"A boat for one person",price:275},
+            {id:2, name:'lifejacket', category:'watersports',discription:" protective and fashionable",price:270},
+            {id:3, name:'thinking cap', category:' soccer',discription:"game soccer",price:375},
+            {id:4, name:' soccerball', category:' soccer',discription:"soccer game",price:275},
+            {id:5, name:' corner flags', category:' soccer',discription:" soccer",price:675},
+            {id:6, name:' stadium', category:' soccer',discription:"A boat for one person",price:275},
+            {id:7, name:'unsteady chair', category:'chess',discription:"A boat for one person",price:175},
+            {id:8, name:' human chess board', category:'chess',discription:"chess game disc",price:275},
+            {id:9, name:' biling biling king', category:'chess',discription:" chess game",price:875},
+
+        ],
+        order:[]
+    }
+}
